ce526ba5f06dedf04d372b721c4693bb6eef6fdf
Alfred <alfredkumar@gmail.com>
2022-08-13 13:28:34 +1000

    Added Basice Theme Files

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+
+/** 
+ *  Contains footer
+*/
+
+?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+
+/** 
+*Contains the header 
+**/
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="">
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+
+    </head>
+<body>
+<!-- Tags close in footer.php -->
